ea1b2c3f67671eb4a2518a23340f77d533e6488f
alamoalonso <sebastian.alamo1@gmail.com>
2022-12-29 12:40:33 +0100

    App with Sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarModule } from 'ng-cdbangular';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarModule } from 'ng-cdbangular';
+import { ButtonModule } from 'ng-cdbangular';
+import { DropdownModule } from 'ng-cdbangular';
+import { CollapseModule } from 'ng-cdbangular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    NavbarModule,
+    ButtonModule,
+    DropdownModule,
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  onHomeClick() {
+
+  }
+
+  onLibraryClick() {
+
+  }
+
+  onUploadNewCaseClick() {
+
+  }
+
+  onMyProfileClick() {
+
+  }
+
+  onHelpClick() {
+
+  }
+
+  onFooterClick() {
+
+  }
+}
