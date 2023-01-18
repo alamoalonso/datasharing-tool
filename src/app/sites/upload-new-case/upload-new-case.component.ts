2917ec72d4c49aa75f430ff1883128b7427604ce
alamoalonso <sebastian.alamo1@gmail.com>
2023-01-18 15:06:45 +0100

    Upload new Case added

--- src/app/sites/upload-new-case/upload-new-case.component.ts
+++ src/app/sites/upload-new-case/upload-new-case.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class UploadNewCaseComponent {
 
+  public title = null;
+  public organizations = null;
+  public industries = null;
+  public year_of_publication = null;
+  public type = null;
+  public maturity_of_processing = null;
+  public source = null;
+  public privacy = null;
+  public pii_relevance = null;
+  public sovereignitiy = null;
+  public timeframe = null;
+  public motivation = null;
+  public scope = null;
+  public reward = null;
+  public payment_model = null;
+  public role = null;
+  public provision = null;
+  public interoperability = null;
+  public access_coordination = null;
+  public frequency = null;
+  public purpose_of_usage = null;
+  public type_of_relationship = null;
+  public benefits = null;
+  public initiating_party = null;
+  public control_authority = null;
+
+
 }
